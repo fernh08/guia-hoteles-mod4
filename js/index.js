1079ae1187f4c264817a170b9ec66cbd45a88b97
Fernando E. Hartl <fernh_08@hotmail.com>
2020-11-14 00:39:16 -0300

    Usando gulp

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function () {
+    $("[data-toggle='tooltip']").tooltip();
+    $("[data-toggle='popover']").popover();
+    $('.carousel').carousel({
+      interval: 3000
+    });
+
+    $('#contacto').on('show.bs.modal', function (e) {
+      console.log('el modal se esta mostrando');
+
+      $('#contactoBtn').removeClass('btn-outline-success');
+      $('#contactoBtn').addClass('btn-primary');
+      $('#contactoBtn').prop('disabled', true);
+    });
+
+    $('#contacto').on('shown.bs.modal', function (e) {
+      console.log('el modal se mostro');
+    });
+
+    $('#contacto').on('hide.bs.modal', function (e) {
+      console.log('el modal se esta ocultando');
+    });
+
+    $('#contacto').on('hidden.bs.modal', function (e) {
+      console.log('el modal esta oculto');
+      $('#contactoBtn').prop('disabled', false);
+    });
+
+  });
